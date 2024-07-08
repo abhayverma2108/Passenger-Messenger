0afdcc5f50556235d6ac910b38560ba4ae35304a
Abhay Verma <abhayv175@gmail.com>
2024-07-08 22:58:47 +0530

    setup user page

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import EmptyState from "../components/EmptyState";
 
 const Users = () => {
-    return (
-        <div>
-            Hello Users!
-        </div>
-    )
+  return ( 
+    <div className="hidden lg:block lg:pl-80 h-full">
+      <EmptyState />
+    </div>
+   );
 }
-
+ 
 export default Users;
