1ccb40ce81ccb61323c47cdc42a21e893468ca51
Abhay Verma <abhayv175@gmail.com>
2024-07-10 09:25:32 +0530

    development complete

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,7 +1,9 @@
-import clsx from "clsx";
+'use client';
+
+import clsx from 'clsx';
 
 interface ButtonProps {
-  type?: "button" | "submit" | "reset" | undefined;
+  type?: 'button' | 'submit' | 'reset' | undefined;
   fullWidth?: boolean;
   children?: React.ReactNode;
   onClick?: () => void;
@@ -11,13 +13,13 @@ interface ButtonProps {
 }
 
 const Button: React.FC<ButtonProps> = ({
-  type = "button",
+  type,
   fullWidth,
   children,
   onClick,
   secondary,
   danger,
-  disabled,
+  disabled
 }) => {
   return ( 
     <button
@@ -25,22 +27,22 @@ const Button: React.FC<ButtonProps> = ({
       type={type}
       disabled={disabled}
       className={clsx(`
-        flex 
-        justify-center 
-        rounded-md 
-        px-3 
-        py-2 
-        text-sm 
-        font-semibold 
-        focus-visible:outline 
-        focus-visible:outline-2 
-        focus-visible:outline-offset-2 
-        `,
-        disabled && 'opacity-50 cursor-default',
-        fullWidth && 'w-full',
-        secondary ? 'text-gray-900' : 'text-white',
-        danger && 'bg-rose-500 hover:bg-rose-600 focus-visible:outline-rose-600',
-        !secondary && !danger && 'bg-purple-500 hover:bg-purple-600 focus-visible:outline-purple-600'
+        flex
+        justify-center
+        rounded-md
+        px-3
+        py-2
+        text-sm
+        font-semibold
+        focus-visible:outline
+        focus-visible:outline-2
+        focus-visible:outline-offset-2
+      `,
+      disabled && "opacity-50 cursor-default",
+      fullWidth && "w-full",
+      secondary ? 'text-gray-900' : 'text-white',
+      danger && "bg-rose-500 hover:bg-rose-600 focus-visible:outline-rose-600",
+      !secondary && !danger && "bg-sky-500 hover:bg-sky-600 focus-visible:outline-sky-600"
       )}
     >
       {children}
--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import clsx from "clsx";
-import { 
-  FieldErrors, 
-  FieldValues, 
-  UseFormRegister 
-} from "react-hook-form";
+import clsx from 'clsx';
+import {
+  FieldErrors,
+  FieldValues,
+  UseFormRegister
+} from 'react-hook-form';
 
 interface InputProps {
   label: string;
@@ -13,30 +13,30 @@ interface InputProps {
   type?: string;
   required?: boolean;
   register: UseFormRegister<FieldValues>,
-  errors: FieldErrors
+  errors: FieldErrors,
   disabled?: boolean;
 }
 
 const Input: React.FC<InputProps> = ({
   label,
   id,
-  register,
+  type,
   required,
+  register,
   errors,
-  type = 'text',
-  disabled,
+  disabled
 }) => {
   return ( 
     <div>
       <label 
-        htmlFor={id} 
         className="
-          block 
-          text-sm 
-          font-medium 
-          leading-6 
+          block
+          text-sm
+          font-medium
+          leading-6
           text-gray-900
         "
+        htmlFor={id}
       >
         {label}
       </label>
@@ -49,24 +49,24 @@ const Input: React.FC<InputProps> = ({
           {...register(id, { required })}
           className={clsx(`
             form-input
-            block 
-            w-full 
-            rounded-md 
-            border-0 
-            py-1.5 
-            text-gray-900 
-            shadow-sm 
-            ring-1 
-            ring-inset 
-            ring-gray-300 
-            placeholder:text-gray-400 
-            focus:ring-2 
-            focus:ring-inset 
-            focus:ring-sky-600 
-            sm:text-sm 
-            sm:leading-6`,
-            errors[id] && 'focus:ring-rose-500',
-            disabled && 'opacity-50 cursor-default'
+            block
+            w-full
+            rounded-md
+            border-0
+            py-1.5
+            text-gray-900
+            shadow-sm
+            ring-1
+            ring-inset
+            ring-gray-300
+            placeholder:text-gray-400
+            focus:ring-2
+            focus:ring-inset
+            focus:ring-sky-600
+            sm:text-sm
+            sm:leading-6`, 
+            errors[id] && "focus:ring-rose-500",
+            disabled && "opacity-50 cursor-default"
           )}
         />
       </div>
